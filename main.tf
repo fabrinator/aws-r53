5b03c73705a08f6b0e9f1386cbb8805df73f3819
Fabri Rivas <info@fabririvas.com>
2024-07-16 14:19:45 -0400

    TEST: access through oidc

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@ provider "aws" {
   region = "us-east-1"
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::621897290135:role/oidc-github-actions"
-    session_name            = "bootcamp-account"
-#     web_identity_token = var.ACTIONS_RUNTIME_TOKEN
+#     session_name            = "SESSION_NAME"
+    web_identity_token = var.ACTIONS_ID_TOKEN_REQUEST_TOKEN
   }
 }
 
@@ -13,3 +13,4 @@ output "out" {
   value = data.aws_caller_identity.this.account_id
 }
 
+variable "ACTIONS_ID_TOKEN_REQUEST_TOKEN" {}
