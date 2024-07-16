982aeb0d70b1f6bf1f0fe44be5cc2701400507b3
Fabri Rivas <info@fabririvas.com>
2024-07-16 17:17:43 -0400

    TEST: access through oidc

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  alias = "bootcamp-account"
   region = "us-east-1"
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::621897290135:role/oidc-github-actions"
@@ -6,10 +7,25 @@ provider "aws" {
   }
 }
 
-data "aws_caller_identity" "this" {}
+provider "aws" {
+  alias = "crc-dev"
+  region = "us-east-1"
+  assume_role_with_web_identity {
+    role_arn                = "arn:aws:iam::912008965710:role/oidc-github-actions"
+    web_identity_token = var.id_token
+  }
+}
+
+data "aws_caller_identity" "this" {
+  provider = aws.bootcamp-account
+}
+
+data "aws_caller_identity" "this2" {
+  provider = aws.bootcamp-account
+}
 
 output "out" {
-  value = data.aws_caller_identity.this.account_id
+  value = [data.aws_caller_identity.this.account_id, data.aws_caller_identity.this2.account_id]
 }
 
 variable "id_token" {}
