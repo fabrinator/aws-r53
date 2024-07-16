b2e1566fbec845b7c870d5677748df92d93cf323
Fabri Rivas <info@fabririvas.com>
2024-07-16 13:36:15 -0400

    TEST: access through oidc

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::621897290135:role/oidc-github-actions"
 #     session_name            = "SESSION_NAME"
-#     web_identity_token_file = "/Users/tf_user/secrets/web-identity-token"
+    web_identity_token = var.TF_VAR_ACTIONS_RUNTIME_TOKEN
   }
 }
 
@@ -12,3 +12,5 @@ data "aws_caller_identity" "this" {}
 output "out" {
   value = data.aws_caller_identity.this.account_id
 }
+
+# variable "ACTIONS_RUNTIME_TOKEN" {}
