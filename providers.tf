dbbf52f264623103a57afd65b493004c25083797
Fabri Rivas <info@fabririvas.com>
2024-07-22 14:13:26 -0400

    Upload prod definition

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aws" {
 #   profile = "infra-shared"
-#   alias = "infra-shared"
+  alias = "infra-shared"
   region = "us-east-1"
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::730335655220:role/oidc-github-actions"
