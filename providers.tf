a5ee3c57b95eb27cc717439b146fe4b540a25c68
Fabri Rivas <info@fabririvas.com>
2024-07-23 10:04:22 -0400

    ADD extra terraform workflow for stage

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-#   profile = "infra-shared"
+#   profile = "crc-dev"
   region = "us-east-1"
   assume_role_with_web_identity {
     role_arn                = "arn:aws:iam::${var.TF_PROVIDER_AWS_ACCOUNT}:role/oidc-github-actions"
